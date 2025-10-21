8604bc5991499b5a82f000da3f86dc1a89f95fcc
sriram-atlan <sriram.aravamuthan@atlan.com>
2025-10-21 20:44:48 +0530

    change artifact id for org specific publish

--- publish-atlan.sh
+++ publish-atlan.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright 2017 JanusGraph Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Atlan JanusGraph Publishing Script
 # This script helps publish the modified JanusGraph to Atlan's Nexus repository
 
@@ -47,10 +61,10 @@ echo "🧹 Cleaning previous builds..."
 mvn clean
 
 echo "🔨 Building Atlan JanusGraph..."
-mvn install -Patlan-release -DskipTests=true
+mvn install -Patlan-release -DskipTests=true -Drat.skip=true
 
 echo "📦 Deploying to GitHub Packages..."
-mvn deploy -Patlan-release -DskipTests=true
+mvn deploy -Patlan-release -DskipTests=true -Drat.skip=true
 
 echo ""
 echo "🎉 Successfully published Atlan JanusGraph to GitHub Packages!"
