018521d4f574f4eedc86e95e27455232ea251a86
sriram-atlan <sriram.aravamuthan@atlan.com>
2025-10-25 03:40:32 +0530

    no-op method

--- janusgraph-solr/src/main/java/org/janusgraph/diskstorage/solr/SolrIndex.java
+++ janusgraph-solr/src/main/java/org/janusgraph/diskstorage/solr/SolrIndex.java
@@ -1264,6 +1264,11 @@ public class SolrIndex implements IndexProvider {
         }
     }
 
+    @Override
+    public void handleMutationFailure(Map<String, Map<String, IndexMutation>> mutations, Throwable cause) {
+        
+    }
+
     /*
     ################# UTILITY METHODS #######################
      */
